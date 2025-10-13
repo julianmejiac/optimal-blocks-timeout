f38f4c4ffa992ebb2655c93e80c35a932aa715c4
julianmejiac <81821063+julianmejiac@users.noreply.github.com>
2025-10-13 01:08:00 -0400

    Update timeopt.py

--- timeopt.py
+++ timeopt.py
@@ -13,9 +13,10 @@ def find_mu(k, epsilon=0.01):
 N=int(input("N, el numero total de votos (por nodo): "))
 h=1440
 lamb=N/h
-k=int(input("k, el cantidad de votos por bloque: "))
+k=int(input("k, la cantidad de votos por bloque: "))
 mu=find_mu(k)
 T=mu/lamb
 print(T)
 eps=poisson.cdf(k - 1, mu)
+
 print(f'Para bloques de tamano k={k},lambda={lamb} votos/min, tenemos que en  T={T} minutos se va a llenar el bloque con una probabilidad de {1-eps}')
